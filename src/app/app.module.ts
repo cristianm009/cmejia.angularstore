c795f41280026ba872f3513bbcdde4a4bbe531b5
Cristian Alveiro Mejia Obando <alveiro09@hotmail.com>
2020-05-04 19:28:59 -0500

    Fix bug with modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,34 +6,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 
-import { ProductComponent} from './product/components/product/product.component';
-import { CartComponent } from './shared/components/cart/cart.component';
-import { ProductsComponent } from './product/components/products/products.component';
-import { ContactComponent } from './contact/components/contact.component';
-import { DemoComponent } from './demo/components/demo/demo.component';
-import { PageNotFoundComponent } from './page-not-found/components/page-not-found/page-not-found.component';
-import { ProductDetailComponent } from './product/components/product-detail/product-detail.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    CartComponent,
-    ProductsComponent,
-    ContactComponent,
-    DemoComponent,
-    PageNotFoundComponent,
-    ProductDetailComponent,
-    LayoutComponent
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    SharedModule,
+    CoreModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
